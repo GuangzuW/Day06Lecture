c0526e5604e51d45e5b0ce19e2454b43bbb9a961
Wang Guangzu <wgz199154@hotmail.com>
2023-01-17 11:31:07 +0800

    fix and cached thread

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -1,5 +1,8 @@
 package sg.edu.nus.iss;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * Hello world!
  */
@@ -13,20 +16,59 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        Thread thread1 = new Thread(new Runnable() {
+    //     Thread thread1 = new Thread(new Runnable() {
 
-            @Override
-            public void run() {
-                for (int i = 0; i < 20; i++) {
-                    System.out.println(Thread.currentThread().getName() + "\tRunnable ....." + i);
-                }
-            }
-        });
-        thread1.start();
+    //         @Override
+    //         public void run() {
+    //             for (int i = 0; i < 20; i++) {
+    //                 System.out.println(Thread.currentThread().getName() +"\tRunnable ....." + i);
+    //             }
+    //         }
+    //     });
+    //     thread1.start();
 
-        MyRunnableImplementation mRI= new MyRunnableImplementation();
-        Thread thread2 = new Thread(mRI);
-        thread2.start();
+        MyRunnableImplementation mRI1= new MyRunnableImplementation("task1");
+        MyRunnableImplementation mRI2= new MyRunnableImplementation("task2");
+        MyRunnableImplementation mRI3= new MyRunnableImplementation("task3");
+        MyRunnableImplementation mRI4= new MyRunnableImplementation("task4");
+        MyRunnableImplementation mRI5= new MyRunnableImplementation("task5");
+        // Thread thread2 = new Thread(mRI);
+        // thread2.start();
+
+        // Thread thread3=new Thread(mRI);
+        // thread3.start();
+
+
+//create thread pull
+
+        // ExecutorService executorService=Executors.newSingleThreadExecutor();
+        // executorService.execute(mRI1);
+        // executorService.execute(mRI2);
+        // executorService.shutdown();
+
+
+
+//new fix thread pull(fix number of thread)
+        // ExecutorService executorService=Executors.newFixedThreadPool(3);
+        // executorService.execute(mRI1);
+        // executorService.execute(mRI2);
+        // executorService.execute(mRI3);
+        // executorService.execute(mRI4);
+        // executorService.execute(mRI5);
+        // executorService.shutdown();
+
+
+//cached thread pull(depend on you computer processor)
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        executorService.execute(mRI1);
+        executorService.execute(mRI2);
+        executorService.execute(mRI3);
+        executorService.execute(mRI4);
+        executorService.execute(mRI5);
+        executorService.shutdown();
     }
+
+
 
 }
